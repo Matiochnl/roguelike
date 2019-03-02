124f737ab174f8c65ddd078f5fae9f0d23bc67c9
Szymon Pluta <satanthesavior>
2019-03-02 15:02:27 +0100

    Rename main to roguelike and refactor it to use data_manager

---
+++ roguelike.py
@@ -0,0 +1,41 @@
+import os
+import ui
+import data_manager
+import time
+
+
+def main_menu():
+    answer = ""
+
+    while not answer:
+        os.system("clear")
+        print(data_manager.load_ascii_art("ascii_art/castle.txt"))
+        answer = ui.main_menu_options()
+
+        if answer == '1':
+            back = ui.get_input("Press Enter to go back to main menu.")
+            answer = ""
+        elif answer == '2':
+            back = ui.get_input("Press Enter to go back to main menu.")
+            answer = ""
+        elif answer == '3':
+            print(data_manager.load_ascii_art("howtoplay.txt"))
+            back = ui.get_input("Press Enter to go back to main menu.")
+            answer = ""
+        elif answer == '4':
+            print(data_manager.load_ascii_art("authors.txt"))
+            back = ui.get_input("Press Enter to go back to main menu.")
+            answer = ""
+        elif answer == '5':
+            quit()
+        else:
+            ui.get_input("You must choose a valid option. Press Enter to go back to main menu.")
+            answer = ""
+
+
+def main():
+    main_menu()
+
+
+if __name__ == "__main__":
+    main()
