d12d13be2ff2833c99a855f22cb746a196ee76f7
Mateusz Ochel <Mateusz.ohel@gmail.com>
2019-03-02 17:02:22 +0100

    Add statustic function in character_creation module

--- character_creation.py
+++ character_creation.py
@@ -1,9 +1,16 @@
 import ui
-
+import os
 # TODO get rid off print functions
-def add_character_stats():
+
+
+def statistic():
     stats = {'hp': 10, 'exp': 0, 'def': 1, 'atc': 1, 'lvl': 1}
-    print_character_statistics(stats)
+    return stats
+
+
+def add_character_stats(statistic):
+    stats = statistic()
+    ui.print_character_statistics(stats)
     points = 5
     while points > 0:
         stat_to_add = input("Enter 'H', 'h', 'D', 'd' or 'A', 'a' to add statistic: ")
@@ -15,7 +22,6 @@ def add_character_stats():
             stats["atc"] += 1
         points -= 1
         os.system("clear")
-        print_character_statistics(stats)
+        ui.print_character_statistics(stats)
         print("Start movement to play")
     return stats
-
