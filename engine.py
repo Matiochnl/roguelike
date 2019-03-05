1b6b17bb4605b39add9a9450f0edd9fb3bacc7b1
Szymon Pluta <satanthesavior>
2019-03-05 13:31:29 +0100

    Add game won ASCII art and printing it

--- engine.py
+++ engine.py
@@ -26,7 +26,7 @@ def start_engine(char_stats, inventory, map_iterator):
     level_map = data_manager.get_maps_from_file(MAPS[map_iterator])
     level_map = movement.place_hero_new_pos(level_map, hero_coordinates)
     ui.print_level_map(level_map, char_stats, inventory)
-        
+
     game_won = True
     while get_char != "q" and game_won:
         get_char = movement.get_char_in_terminal()
@@ -39,19 +39,21 @@ def start_engine(char_stats, inventory, map_iterator):
                 os.system("clear")
                 game_won = False
                 minigames.hotcold(map_iterator, char_stats, character)
+
             if char_stats["HP"] <= 0:
                 os.system("clear")
                 game_won = False
                 print(data_manager.load_ascii_art("ascii_art/game_over.txt"))
-                
-            if game_won != False and map_iterator <= 2:
+
+            if game_won is not False and map_iterator <= 2:
                 level_map = movement.map_with_updated_hero_pos(get_char, level_map, hero_coordinates, new_hero_coordinates)
                 ui.print_level_map(level_map, char_stats, inventory)
+
             if map_iterator < 2 and character == "D":
                 char_stats["LVL"] = char_stats["LVL"] + 1
                 map_iterator += 1
                 level_map = data_manager.get_maps_from_file(MAPS[map_iterator])
-                hero_coordinates = HERO_COORDINATES[map_iterator]   
+                hero_coordinates = HERO_COORDINATES[map_iterator]
         else:
             level_map = movement.map_with_updated_hero_pos(get_char, level_map, hero_coordinates, new_hero_coordinates)
             ui.print_level_map(level_map, char_stats, inventory)
